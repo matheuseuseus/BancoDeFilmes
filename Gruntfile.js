3da10df88763da78c19ac6c56dda85c83007baf9
matheuseuseus <matheus.kder@gmail.com>
2018-11-03 22:35:25 -0300

    Correção do jquery

--- Gruntfile.js
+++ Gruntfile.js
@@ -387,12 +387,7 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
-        }, {
-          expand: true,
-          cwd: 'bower_components/bootstrap/dist',
-          src: 'fonts/*',
-          dest: '<%= yeoman.dist %>'
-        }]
+        }],
       },
       styles: {
         expand: true,
